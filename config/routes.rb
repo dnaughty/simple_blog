7f3827f778ef0b87800051b690f778c93fad38a7
Denis Naughton <mjolinor75@hotmail.com>
2021-02-28 18:19:51 -0500

    Creating elements part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   
-  resources :elements
+  
   devise_for :authors
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrailso.rg/routing.html
   root 'home#index'
   scope module: 'authors' do 
-    resources :posts
+    resources :posts do
+    	resources :elements
+    end
   end
 end
